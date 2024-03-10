acc5c625e96c406b34deaeda3a1fe3d6bda42faa
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 23:40:43 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -2,9 +2,7 @@
   var date=new Date();
   var hour=date.getHours();
   if(hour>18){
-    var tags = document.getElementById('main').getElementsByTagName('section');
-    for (var i = 0; i < tags.length; ++i) {
-      tags[i].getElementsByTagName('section')[0].style.color = '#fcfcfc';
-    }
+    document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].style.color = '#fafafa';
+    document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color = '#dfdfdf';
   }
 })()
