d9df00c75785280b41ffdd23e93049ceddbcbf67
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 23:23:22 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -3,7 +3,7 @@
   var hour=date.getHours();
   if(hour>18){
     var tags = document.getElementById('main').getElementsByTagName('section');
-    for (var i = 0; i < arr.length; ++i) {
+    for (var i = 0; i < tags.length; ++i) {
       tags[i].getElementsByTagName('section')[0].style.color = '#fcfcfc';
     }
   }
