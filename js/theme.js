67bf094e87769d7a8c87bd9a2f4ecf914890d4b8
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 20:58:03 +0800

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,8 +1,8 @@
 function changeTheme() {
   var date = new Date();
   var hour = date.getHours();
-  document.body.style.color = "#fcfcfc";
-  document.body.style.backgroundColor = "#4b4b4b";
+  document.getElementById('top').style.color = "#fcfcfc";
+  document.getElementById('top').style.backgroundColor = "#4b4b4b";
 }
 
 changeTheme();
