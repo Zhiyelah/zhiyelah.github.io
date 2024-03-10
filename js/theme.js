6bcf0994727ab1e181718dbd76e845bdbe14ec47
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 17:40:27 +0800

    Init

--- js/theme.js
+++ js/theme.js
@@ -2,13 +2,13 @@ function changeTheme() {
   var date = new Date();
   var hour = date.getHours();
   if (false) {
-    document.getElementById("elementId").style.color = "#4b4b4b";
-    document.getElementById("elementId").style.background-color = "#fcfcfc";
+    document.getElementById("main").style.color = "#4b4b4b";
+    document.getElementById("main").style.background-color = "#fcfcfc";
   } else {
-    document.getElementById("elementId").style.color = "#fcfcfc";
-    document.getElementById("elementId").style.background-color = "#4b4b4b";
+    document.getElementById("main").style.color = "#fcfcfc";
+    document.getElementById("main").style.background-color = "#4b4b4b";
   }
 }
 
-changeTheme();
+window.onload = changeTheme;
 
