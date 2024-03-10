4fd36aa4a11409ad747143843e25d355e60495d7
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 17:30:14 +0800

    Init

---
+++ js/theme.js
@@ -0,0 +1,14 @@
+function changeTheme() {
+  var date = new Date();
+  var hour = date.getHours();
+  if (hour < 18) {
+    document.getElementById("elementId").style.color = "#4b4b4b";
+    document.getElementById("elementId").style.background-color = "#fcfcfc";
+  } else {
+    document.getElementById("elementId").style.color = "#fcfcfc";
+    document.getElementById("elementId").style.background-color = "#4b4b4b";
+  }
+}
+
+window.onload = changeTheme;
+
