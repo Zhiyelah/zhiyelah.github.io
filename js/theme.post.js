425d25e0fc22595d1ea42dcf493a947d6249107d
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-10 23:55:06 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -2,7 +2,14 @@
   var date=new Date();
   var hour=date.getHours();
   if(hour>18){
-    document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#fafafa';
-    document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color = '#dfdfdf';
+    try {
+      document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#f5f5f5';
+      document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color = '#dfdfdf';
+    } catch (error) {}
+
+    try {
+      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].style.color = '#f5f5f5';
+      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color = '#dfdfdf';
+    } catch (error) {}
   }
 })()
