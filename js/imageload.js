d3d0db4d039b362c3f6114efb556cc5e7d805358
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-12 13:09:53 +0800

    Delete

--- js/imageload.js
+++
@@ -1,8 +0,0 @@
-(function loadImg() {
-  let img = new Image();
-  img.src = '../images/bg.jpg';
-  img.onload = function(){
-    document.getElementById('header').style.backgroundImage = 'url(\"${img.src}\")';
-  };
-})()
-
