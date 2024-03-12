69b58dc0002e0e2ccaf8999b16e55393ce74abc2
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-12 12:52:57 +0800

    Update imageload.js

--- js/imageload.js
+++ js/imageload.js
@@ -1,6 +1,6 @@
 (function loadImg() {
   let img = new Image();
-  img.src = 'url(\"images/overlay.png\"), url(\"../images/bg.jpg\")';
+  img.src = '../images/bg.jpg';
   img.onload = function(){
     document.getElementById('header').style.backgroundImage = img.src;
   };
