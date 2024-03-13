4851699d66f2845b4998405b5c1c73aab9043cfc
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-13 22:07:05 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -5,11 +5,12 @@
     try {
       document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#f5f5f5';
       document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color = '#cfcfcf';
-    } catch (error) {}
+      document.getElementById('main').getElementsByTagName('section')[1].getElementsByTagName('div')[0].getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].style.color = '#f1f1f1';
+    } catch (e) {}
 
     try {
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#f5f5f5';
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color = '#cfcfcf';
-    } catch (error) {}
+    } catch (e) {}
   }
 })()
