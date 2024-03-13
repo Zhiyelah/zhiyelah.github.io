859ff75b84c4d04ca677a3f172b02372d04bc8ed
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-13 22:41:50 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -1,16 +1,1 @@
-(function changeTheme(){
-  var date=new Date();
-  var hour=date.getHours();
-  if(hour>18||hour<5){
-    try {
-      document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#f5f5f5';
-      document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color = '#cfcfcf';
-      document.getElementById('main').getElementsByTagName('section')[1].getElementsByTagName('div')[0].getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].getElementsByTagName('a')[0].style.color = '#f1f1f1';
-    } catch (e) {}
-
-    try {
-      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color = '#f5f5f5';
-      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color = '#cfcfcf';
-    } catch (e) {}
-  }
-})()
+(function(){var date=new Date();var hour=date.getHours();if(hour>18||hour<5){try{document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';document.getElementById('main').getElementsByTagName('section')[0].getElementsByTagName('section')[0].style.color='#cfcfcf';}catch(e){}try{document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#cfcfcf';}catch(e){}}})()
