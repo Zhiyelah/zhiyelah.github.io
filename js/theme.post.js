3b14e442b970daaa3de7c1ff9307ef3b6c8467a1
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-14 22:30:50 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -5,23 +5,9 @@
     try{
       document.getElementById('primary').getElementsByTagName('h1')[0].style.color='#f5f5f5';
       document.getElementById('primary').getElementsByTagName('h3')[0].style.color='#f5f5f5';
-      var i=0;
-      try{
-        while(1){
-          document.getElementsByClassName('hover')[i].style.color='#f2f2f2';
-          i++;
-        }
-      }catch(e){}
     }catch(e){}
     try{
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';
-      for(let hs in ['h1','h2','h3','h4']){
-        try{
-          document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].getElementsByTagName(hs)[0].style.color='#f5f5f5';
-        }catch(e){
-          continue;
-        }
-      }
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#cfcfcf';
     }catch(e){}
   }
