0f5305820f7bf33b5f98a1bd87ed6a29890cc03e
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-15 20:22:02 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -2,19 +2,29 @@
   var date=new Date();
   var hour=date.getHours();
   if(hour>18||hour<5){
+    function hoverIn() {
+      this.style.color='#e8e8e8';
+    }
     try{
-      document.getElementById('primary').getElementsByTagName('h1')[0].style.color='#f8f8f8';
-      document.getElementById('primary').getElementsByTagName('h3')[0].style.color='#f5f5f5';
+      document.getElementById('primary').getElementsByTagName('h1')[0].style.color='#dedede';
+      document.getElementById('primary').getElementsByTagName('h3')[0].style.color='#d1d1d1';
+      for (let hx in document.getElementsByClassName('fade')) {
+        hx.addEventListener('mouseenter', hoverIn);
+      }
     }catch(e){}
+
     try{
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#cfcfcf';
+
       try{
-        document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('footer')[0].getElementsByTagName('div')[0].getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].getElementsByTagName('a')[0].style.color='#f2f2f2';
+        document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('footer')[0].getElementsByTagName('div')[0].getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].getElementsByTagName('a')[0].style.color='#e8e8e8';
       }catch(e){}
       try{
-        document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('footer')[0].getElementsByTagName('div')[0].getElementsByTagName('article')[1].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].getElementsByTagName('a')[0].style.color='#f2f2f2';
+        document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('footer')[0].getElementsByTagName('div')[0].getElementsByTagName('article')[1].getElementsByTagName('header')[0].getElementsByTagName('h2')[0].getElementsByTagName('a')[0].style.color='#e8e8e8';
       }catch(e){}
+
+      
     }catch(e){}
   }
 })()
