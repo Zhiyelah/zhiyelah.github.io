7140432f9f5e8543a97c8d2ee57b36450150cb9b
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-15 23:06:07 +0800

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,2 +1,2 @@
-(function(){var date=new Date();var hour=date.getHours();if(hour>18||hour<5){document.getElementById('top').style.background='linear-gradient(to left, #313131, #4f4f4f)';}else{document.getElementById('top').style.background='linear-gradient(to left, #fafafa, #ececec)';}document.oncontextmenu=function(){return false;};})()
+(function(){var date=new Date();var hour=date.getHours();if(hour>18||hour<5){document.getElementById('top').style.background='linear-gradient(-23deg, #4f4f4f, #313131)';}else{document.getElementById('top').style.background='linear-gradient(-23deg, #ececec, #fafafa)';}document.oncontextmenu=function(){return false;};})()
 // Theme switch. Zhiyesh
