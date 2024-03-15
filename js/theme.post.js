b2d9b47346d06cb9f72220c79230cde411ba3c81
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-15 21:14:25 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -16,13 +16,13 @@
         };
         fades[i].onmouseout = function() {
           this.style.color='#9b9b9b';
-        }
+        };
       }
     }catch(e){}
 
     try{
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';
-      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#cfcfcf';
+      document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#d6d6d6';
 
       var hovers = document.getElementsByClassName('hover');
       for (var i = 0; i < hovers.length; ++i) {
@@ -35,7 +35,7 @@
         };
         fades[i].onmouseout = function() {
           this.style.color='#9b9b9b';
-        }
+        };
       }
     }catch(e){}
   }
