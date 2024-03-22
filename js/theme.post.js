9f367b47ec3e7a0202cc2b7183d7675ba8a986ee
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-22 22:12:04 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -3,8 +3,14 @@
   var hour=date.getHours();
   if(hour>18||hour<5){
     try{
-      document.getElementById('primary').getElementsByTagName('h1')[0].style.color='#dedede';
-      document.getElementById('primary').getElementsByTagName('h3')[0].style.color='#d1d1d1';
+      var h1s = document.getElementById('primary').getElementsByTagName('h1');
+      for (var i = 0; i < h1s.length; ++i) {
+        h1s[i].style.color='#dedede';
+      }
+      var h3s = document.getElementById('primary').getElementsByTagName('h3');
+      for (var i = 0; i < h3s.length; ++i) {
+        h3s[i].style.color='#d1d1d1';
+      }
       var hovers = document.getElementsByClassName('hover');
       for (var i = 0; i < hovers.length; ++i) {
         hovers[i].style.color='#e1e1e1';
@@ -25,6 +31,10 @@
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#d6d6d6';
 
+      var codes = document.getElementsByClassName('language-plaintext highlighter-rouge');
+      for (var i = 0; i < codes.length; ++i) {
+        codes[i].style.backgroundColor='#4b4b4b';
+      }
       var hovers = document.getElementsByClassName('hover');
       for (var i = 0; i < hovers.length; ++i) {
         hovers[i].style.color='#e1e1e1';
