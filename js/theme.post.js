9d94f2d4d79aa1e15532b903340b629995b1d6bf
Zhiyesh <160143251+Zhiyesh@users.noreply.github.com>
2024-03-27 20:48:02 +0800

    Update theme.post.js

--- js/theme.post.js
+++ js/theme.post.js
@@ -4,37 +4,37 @@
   if(hour>18||hour<5){
     try{
       try{
-        var h1s = document.getElementById('primary').getElementsByTagName('h1');
+        var h1s = document.getElementsByTagName('h1');
         for (var i = 0; i < h1s.length; ++i) {
           h1s[i].style.color='#dedede';
         }
       }catch(e){}
       try{
-        var h2s = document.getElementById('primary').getElementsByTagName('h2');
+        var h2s = document.getElementsByTagName('h2');
         for (var i = 0; i < h2s.length; ++i) {
           h2s[i].style.color='#dcdcdc';
         }
       }catch(e){}
       try{
-        var h3s = document.getElementById('primary').getElementsByTagName('h3');
+        var h3s = document.getElementsByTagName('h3');
         for (var i = 0; i < h3s.length; ++i) {
           h3s[i].style.color='#d1d1d1';
         }
       }catch(e){}
       try{
-        var h4s = document.getElementById('primary').getElementsByTagName('h4');
+        var h4s = document.getElementsByTagName('h4');
         for (var i = 0; i < h4s.length; ++i) {
           h4s[i].style.color='#cfcfcf';
         }
       }catch(e){}
       try{
-        var h5s = document.getElementById('primary').getElementsByTagName('h5');
+        var h5s = document.getElementsByTagName('h5');
         for (var i = 0; i < h5s.length; ++i) {
           h5s[i].style.color='#cdcdcd';
         }
       }catch(e){}
       try{
-        var h6s = document.getElementById('primary').getElementsByTagName('h6');
+        var h6s = document.getElementsByTagName('h6');
         for (var i = 0; i < h6s.length; ++i) {
           h6s[i].style.color='#cacaca';
         }
@@ -64,6 +64,42 @@
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('header')[0].getElementsByTagName('h1')[0].style.color='#f5f5f5';
       document.getElementById('main').getElementsByTagName('article')[0].getElementsByTagName('section')[0].style.color='#d6d6d6';
 
+      try{
+        var h1s = document.getElementsByTagName('h1');
+        for (var i = 0; i < h1s.length; ++i) {
+          h1s[i].style.color='#dedede';
+        }
+      }catch(e){}
+      try{
+        var h2s = document.getElementsByTagName('h2');
+        for (var i = 0; i < h2s.length; ++i) {
+          h2s[i].style.color='#dcdcdc';
+        }
+      }catch(e){}
+      try{
+        var h3s = document.getElementsByTagName('h3');
+        for (var i = 0; i < h3s.length; ++i) {
+          h3s[i].style.color='#d1d1d1';
+        }
+      }catch(e){}
+      try{
+        var h4s = document.getElementsByTagName('h4');
+        for (var i = 0; i < h4s.length; ++i) {
+          h4s[i].style.color='#cfcfcf';
+        }
+      }catch(e){}
+      try{
+        var h5s = document.getElementsByTagName('h5');
+        for (var i = 0; i < h5s.length; ++i) {
+          h5s[i].style.color='#cdcdcd';
+        }
+      }catch(e){}
+      try{
+        var h6s = document.getElementsByTagName('h6');
+        for (var i = 0; i < h6s.length; ++i) {
+          h6s[i].style.color='#cacaca';
+        }
+      }catch(e){}
       var codes = document.getElementsByClassName('language-plaintext highlighter-rouge');
       for (var i = 0; i < codes.length; ++i) {
         codes[i].style.backgroundColor='#494949';
