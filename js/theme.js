14791b731e12a6795560998e8b064cdcb9147663
Zhiyelah <160143251+Zhiyelah@users.noreply.github.com>
2025-05-23 11:42:08 +0800

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,2 +1,2 @@
 (function(){var date=new Date();var hour=date.getHours();if(hour>18||hour<5){document.getElementById('top').style.background='linear-gradient(63deg,#3a3a3a,#313131)';}else{document.getElementById('top').style.background='linear-gradient(63deg,#f2f2f2,#fefefe)';}document.oncontextmenu=function(){return false;};})()
-// Theme switch. Zhiyesh
+// Theme switching.
